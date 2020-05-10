4b036e789c42694cef737ba59909411126fa3304
Özge Kocaoğlu <38084944+OzgeKocaoglu@users.noreply.github.com>
2020-05-10 07:02:14 +0300

    First ui panels.
    main panel, search panel, border panel...

---
+++ Service Adjustment Application/Assets/Scripts/ASW_Manager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Amazon;
+
+public class ASW_Manager : MonoBehaviour
+{
+    private void Awake()
+    {
+        UnityInitializer.AttachToGameObject(this.gameObject);
+    }
+}
